91bccb63cf79b858af09fbe8dbbd0f151f17a4af
Murtrag <lukasz.czernik@gmail.com>
2018-11-19 21:03:36 +0100

    changed loop to string multiplication

--- exercise_1.py
+++ exercise_1.py
@@ -6,5 +6,5 @@ mul = input('Enter number of copies: ')
 current_year = datetime.datetime.today()
 hundret_at = current_year + datetime.timedelta(days=365*(100-int(age)))
 
-for _ in range(int(mul)):
-	print("Your are going to be 100yrs old in {}".format(hundret_at.year))
+
+print(int(mul)*"Your are going to be 100yrs old in {} \n".format(hundret_at.year))
