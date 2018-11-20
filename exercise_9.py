690d8a3413c2dc0e60b1979d9b4aae62e8621a5e
Murtrag <lukasz.czernik@gmail.com>
2018-11-20 12:17:33 +0100

    added extra solution for exercise 9

--- exercise_9.py
+++ exercise_9.py
@@ -1,14 +1,18 @@
 from random import randint
 
 number = randint(1,9)
-
+count = 0
 while True:
-	guess = int(input("guess what is the number: "))
+	count+=1
+	guess = input("guess what is the number: ")
+	if guess == "exit":
+		break
+	guess = int(guess)
 
 	if guess<number:
 		print("to low")
 	elif guess == number:
-		print("Thats it!")
+		print("Thats it! You've guessed it in {} tries".format(count))
 		break
 	else:
 		print("To high!")
