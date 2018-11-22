da92f845822ec7b21542ab507d55a5fa9e2e82e3
Murtrag <lukasz.czernik@gmail.com>
2018-11-22 18:12:19 +0100

    solution for exercise 24

---
+++ exercise_24.py
@@ -0,0 +1,11 @@
+def create_board(dim):
+	line = "-"+"---"*dim
+	delimiter = "|"+"  |"*dim
+	for level in range(dim):
+		print(line)
+		print(delimiter)
+	else:
+		print(line)
+
+
+print(create_board(4))
