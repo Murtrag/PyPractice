04febe9c03d26141951a17ccc24d22f24c3e3dd4
Łukasz Czernik <lukasz.czernik@gmail.com>
2022-06-02 14:05:42 +0200

    Update oop_exercise_6.py

--- oop_exercise_6.py
+++ oop_exercise_6.py
@@ -1,15 +1,22 @@
 import re
 
 class Palindrome:
-    def __init___(self, text:str) -> None:
+    def __init__(self, text:str) -> None:
         self.text :str = text
 
     def __strip_redundand(self, text: str) -> str:
-        return re.sub("\A{1}", "", text)
+        return re.sub("\W{1}", "", text)
+    
+    def is_palindrome(self, strip_redundand=True):
+        if strip_redundand:
+            striped_text: str = self.__strip_redundand(
+                self.text
+            )
+            return striped_text == striped_text[::-1]
+        return self.text == self.text[::-1]
 
-    @property
-    def is_palindrome(self):
-        striped_text: str = self.__strip_redundand(
-            self.text
+if __name__ == "__main__":
+    palindrome = Palindrome("ala v ala")
+    print(
+        palindrome.is_palindrome(False)
         )
-        return
