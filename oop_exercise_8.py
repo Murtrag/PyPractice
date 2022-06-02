915f55c60114385abe151c0e73de4674bce96a4a
Łukasz Czernik <lukasz.czernik@gmail.com>
2022-06-02 14:20:44 +0200

    Create oop_exercise_8.py

---
+++ oop_exercise_8.py
@@ -0,0 +1,21 @@
+from random import choice
+
+class RockPaperScissors:
+  points: int = 0
+  round_: int = 0
+    
+  def __init__(self, rounds: int, difficulty: int) -> None:
+    self.rounds : int = rounds
+    self.difficulty : int = difficulty
+      
+  @property
+  def __get_computer_move(self):
+    pass
+  
+  def __check_winner(self, a_move : string, b_move: string)
+   
+  
+  
+if __name__ == "__main__":
+  rps = RockPaperScissors()
+  
