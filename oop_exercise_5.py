e259fd538f5c444dcc6d90950260cdaff443b4ff
Łukasz Czernik <lukasz.czernik@gmail.com>
2022-06-02 13:13:42 +0200

    Update oop_exercise_5.py

--- oop_exercise_5.py
+++ oop_exercise_5.py
@@ -1,6 +1,18 @@
 from collections.abc import Generator
 
-class 
-            
-            
-        
+class Overlaps:
+    def __init__(self, list_1, list_2) -> None:
+        self.list_1: list = list_1
+        self.list_2: list = list_2
+    
+    def get_overlaps(self) -> set:
+        return set(self.list_1).intersection(set(self.list_2))
+    
+    def present(self) -> None:
+        print("Overlaps are: ")
+        for x in self.get_overlaps():
+            print(f"- {x}")
+
+o = Overlaps([1,2,3,4,5,6,7,8], [11,10,9,8, 111, 7])
+
+o.present()
