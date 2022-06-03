6bbb97696d4f600fdd33087fe6d661d970cd5c41
Łukasz Czernik <lukasz.czernik@gmail.com>
2022-06-03 07:58:35 +0200

    Update oop_exercise_9.py

--- oop_exercise_9.py
+++ oop_exercise_9.py
@@ -1,4 +1,6 @@
+from __future__ import annotations
 import random
+
 
 class RandomGame:
   def __init__(self) -> None:
@@ -6,28 +8,32 @@ class RandomGame:
     
   def is_correct(self, number: int):
     return number == self.guess_num
+    
+  def evaluate(self, answear: str) -> str:
+      pass
   
   def restart_guess_num(self) -> None:
     self.guess_num : int = random.randint(1,9)
+    
+
       
 
 def game():
   rg : RandomGame = RandomGame()
-  print("New game, try to guess a number from <1;9>")
   while True:
-    number : int = int(input("What is your guess? "))
-    if rg.is_correct(number):
-      print("Good job that is a correct answear!")
-    print("Nop is ")
-      
-    
-    
-    
-    
-    
-    
-# New game, try to guess a number from <1;9>
-#What is your guess? 4
-#Nop is too small
-#...
+      print("New game, try to guess a number from <1;9>")
+      rg.restart_guess_num()
+      while True:
+        answear : str = input("What is your guess? ")
+        evaluation: str = rg.evaluate(answear)
+        if rg.is_correct(int(answear)):
+          print("Good job that is a correct answear!")
+          break
+        print("Nop is ")
+
+if __name__ == "__main__":
+    game()
+
+
+
 
