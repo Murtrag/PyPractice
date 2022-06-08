ea6c27f72ad31f562a637b7367a0d0dcad7c62f1
Łukasz Czernik <lukasz.czernik@gmail.com>
2022-06-08 16:50:31 +0200

    Create Passgen.py

---
+++ Passgen.py
@@ -0,0 +1,27 @@
+class PassGenContext:
+    def __init__(self) -> None:
+        self.__length = length
+        self.__chars = chars
+        self.__pass_phrase = pass_phrase
+
+    @property
+    def length(self) -> strategy:
+        return self.__length
+
+
+    @length.setter
+    def set_length(self, length) -> Strategy:
+        self.__length = length
+
+    @property
+    def chars(self) -> strategy:
+        return self.__chars
+
+    @length.setter
+    def set_chars(self, chars) -> Strategy:
+        self.__chars = chars
+
+
+#Strategies
+class Strategy:
+    def __init__(self) -> None
