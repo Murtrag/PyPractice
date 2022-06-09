bf68a7accd9af583349accde1c0ebd8b3565384b
Łukasz Czernik <lukasz.czernik@gmail.com>
2022-06-09 20:22:18 +0200

    Update Passgen.py

--- Passgen.py
+++ Passgen.py
@@ -1,27 +1,46 @@
 class PassGenContext:
     def __init__(self) -> None:
-        self.__length = length
-        self.__chars = chars
-        self.__pass_phrase = pass_phrase
+        self.__strategies = list()
 
     @property
-    def length(self) -> strategy:
-        return self.__length
+    def strategies(self) -> list:
+        return self.__strategies
 
+    @strategies.setter
+    def set_strategies(self, strategies) -> Strategy:
+        self.__strategies = strategies
 
-    @length.setter
-    def set_length(self, length) -> Strategy:
-        self.__length = length
-
-    @property
-    def chars(self) -> strategy:
-        return self.__chars
-
-    @length.setter
-    def set_chars(self, chars) -> Strategy:
-        self.__chars = chars
+    def get_password(self) -> str:
+        for strategy in self.__strategies:
+            strategy.transform_password(pass)
+        return pass
 
 
 #Strategies
 class Strategy:
     def __init__(self) -> None
+        pass
+
+    def transform_password(self, pass) -> str:
+        return password
+
+class TextLength(Strategies):
+    def __init_(self, length) -> None:
+        password
+
+class TextChars(Strategies):
+    def __init__self, length) -> None:
+        password
+
+class TextPhrase(Strategies):
+    def __init_(self, length) -> None:
+        pass
+
+ class TextPokemon(Strategy):
+    def __init__(self) -> None:
+        pass
+    def transform_password(self, password) -> str:
+        return ''.join(
+             x.upper() for x in password
+             if x%2==0
+        )
