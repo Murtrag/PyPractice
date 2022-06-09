ccf901521aa79b0a4c0133d2515fbbefe5fa715a
Łukasz Czernik <lukasz.czernik@gmail.com>
2022-06-09 22:22:50 +0200

    Update Passgen.py

--- Passgen.py
+++ Passgen.py
@@ -1,43 +1,51 @@
 class PassGenContext:
     def __init__(self) -> None:
         self.__strategies = list()
+        self.__password = ""
 
     @property
     def strategies(self) -> list:
         return self.__strategies
 
     @strategies.setter
-    def set_strategies(self, strategies) -> Strategy:
+    def set_strategies(self, strategies) -> None:
         self.__strategies = strategies
 
     def get_password(self) -> str:
         for strategy in self.__strategies:
-            strategy.transform_password(pass)
-        return pass
+            self.__passwrod = strategy.transform_password(self.__password)
+        return self.__passwrod or "dupa"
 
 
 #Strategies
 class Strategy:
-    def __init__(self) -> None
+    def __init__(self) -> None:
         pass
 
-    def transform_password(self, pass) -> str:
+    def transform_password(self, password) -> str:
+        print("tr", password)
         return password
 
-class TextLength(Strategies):
-    def __init_(self, length) -> None:
-        password
-
-class TextChars(Strategies):
-    def __init__self, length) -> None:
-        password
-
-class TextPhrase(Strategies):
+class TextLength(Strategy):
     def __init_(self, length) -> None:
         pass
+    def transform_password(self, password) -> str:
+        return password + "length"
 
- class TextPokemon(Strategy):
-    def __init__(self) -> None:
+class TextChars(Strategy):
+    def __init__(self, length) -> None:
+        pass
+    def transform_password(self, password) -> str:
+        return password + "chars"
+
+class TextPhrase(Strategy):
+    def __init__(self, phrase) -> None:
+        pass
+    def transform_password(self, password) -> str:
+        return password + "phrase"
+
+class TextPokemon(Strategy):
+    def __init__(self, boolean) -> None:
         pass
     def transform_password(self, password) -> str:
         return ''.join(
@@ -47,9 +55,11 @@ class TextPhrase(Strategies):
 
 if __name__ == "__main__":
      pgc= PassGenContext()
-     pgc.set_strategies(
+     pgc.set_strategies= [
          TextChars(8),
          TextPhrase("krowi placek"),
          TextPokemon(2)
-     )
+     ]
+     print(
      pgc.get_password()
+     )
